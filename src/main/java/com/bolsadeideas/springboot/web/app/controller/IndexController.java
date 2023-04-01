1e60aa8426635778b9e163942fc788ef99f9a340
JLAGUbuntu <unixluisx@protonmail.ch>
2023-03-31 18:14:30 -0600

    Implementacion de la interfaz para la inyeccion de dependencias

--- src/main/java/com/bolsadeideas/springboot/web/app/controller/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.bolsadeideas.springboot.web.app.controller;
 
+import com.bolsadeideas.springboot.web.app.models.service.IServicio;
 import com.bolsadeideas.springboot.web.app.models.service.MiServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
     @Autowired
-    private MiServicio servicio;
+    private IServicio servicio;
 
     @GetMapping({"/", "/index"})
     public String index(Model model) {
